596109c9dcce9bc78514e06beef10dff5c9d9c3f
Matthew Joseph <matthew.j.joe@gmail.com>
2022-10-26 23:39:38 -0400

    added http methods options

--- pages/components/toolbar.tsx
+++ pages/components/toolbar.tsx
@@ -1,3 +1,15 @@
+import styles from "./../../styles/toolbar.module.css";
+
 export const Toolbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <select className={styles.methodsContainer}>
+        <option>GET</option>
+        <option>POST</option>
+        <option>PUT</option>
+        <option>DELETE</option>
+        <option>UPDATE</option>
+      </select>
+    </div>
+  );
 };
