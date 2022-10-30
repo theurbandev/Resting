495b2986d30cfa68d35c87d8f7c89735817cd9db
Matthew Joseph <matthew.j.joe@gmail.com>
2022-10-29 23:31:46 -0400

    added buttons to (activity) http response section

--- pages/components/activity.tsx
+++ pages/components/activity.tsx
@@ -5,8 +5,22 @@ export const Activity = () => {
     <div className={`flex flex-col h-full ${styles.activity}`}>
       <div className={`${styles.activitySettings}`}>activity settings</div>
 
-      <div className={`${styles.activityResponse}`}>
-        <h1>Response</h1>
+      <div
+        className={`flex flex-row flex justify-between ${styles.activityResponse}`}
+      >
+        <div className={` flex flex-row ${styles.responseToggles}`}>
+          <h3>Body</h3>
+          <h3>Cookies</h3>
+          <h3>Headers</h3>
+        </div>
+
+        <div className={` flex flex-row ${styles.formatButtons}`}>
+          <h3>JSON</h3>
+          <h3>HTML</h3>
+          <h3>Preview</h3>
+          <h3>Raw</h3>
+          <h3>XML</h3>
+        </div>
       </div>
     </div>
   );
